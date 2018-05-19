e19489cc6cefb0d28db261c105d30eabcb1d6003
lkmc2 <lkmc2@163.com>
2018-05-20 01:04:35 +0800

    编写店铺前端js文件

---
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -0,0 +1,64 @@
+$(function () {
+    var initUrl = '/o2o_ssm/shop/getshopinitinfo';
+    var registerShopUrl = '/o2o_ssm/shopadmin/registershop';
+    alert(initUrl);
+    getShopInitInfo();
+    // 获取店铺初始化信息
+    function getShopInitInfo() {
+        $.getJSON(initUrl, function (data) {
+            if (data.success) {
+                var tempHtml = '';
+                var tempAreaHtml = '';
+                data.shopCategoryList.map(function (item, index) {
+                    tempHtml += '<option data-id="' + item.shopCategoryId + '">'
+                        + item.shopCategoryName + '</option>';
+                });
+                data.areaList.map(function (value, index) {
+                    tempAreaHtml += '<option data-id="' + item.areaId + '">'
+                        + item.areaName + '</option>';
+                });
+                // 添加店铺分类下拉选项
+                $('#shop-category').html(tempHtml);
+                // 添加区域下拉选项
+                $('#area').html(tempAreaHtml);
+            }
+        });
+        // 设置提交按钮点击事件
+        $('#submit').click(function () {
+            var shop = {};
+            shop.shopName = $('#shop-name').val();
+            shop.shopAddr = $('#shop-addr').val();
+            shop.phone = $('#shop-phone').val();
+            shop.shopDesc = $('#shop-desc').val();
+            shop.shopCategoryId = {
+                shopCategoryId: $('#shop-category').find('option').not(function () {
+                    return !this.selected;
+                }).data('id')
+            };
+            shop.area = {
+                areaId: $('#area').find('option').not(function () {
+                    return !this.selected;
+                }).data('id')
+            };
+            var shopImg = $('#shop-img')[0].files[0];
+            var formData = new FormData();
+            formData.append('shopImg', shopImg);
+            formData.append('shopStr', JSON.stringify(shopImg));
+            $.ajax({
+                url: registerShopUrl,
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    if (data.success) {
+                        $.toast('提交成功！');
+                    } else {
+                        $.toast('提交失败！' + data.errMsg);
+                    }
+                }
+            })
+        });
+    }
+});
